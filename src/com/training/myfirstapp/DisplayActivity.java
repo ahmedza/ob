77428b3c0f83d767e78a72840cac08ac37fa53b1
unknown <S706826@PC3031355021.corp.emirates.com>
2014-12-29 15:24:17 +0400

    committing initial files

---
+++ src/com/training/myfirstapp/DisplayActivity.java
@@ -0,0 +1,27 @@
+package com.training.myfirstapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class DisplayActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		System.out.println("In On Create Display");
+		setContentView(R.layout.display_activity_main);
+		TextView tv = (TextView) findViewById(R.id.disp_tv);
+		tv.setText(getIntent().getStringExtra("MESSAGE"));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		System.out.println("In Options Display");
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ src/com/training/myfirstapp/MainFirstActivity.java
@@ -0,0 +1,38 @@
+package com.training.myfirstapp;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainFirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("In On create");
+        setContentView(R.layout.activity_main_first);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_first, menu);
+        System.out.println("In Create Menu Options");
+        return true;
+    }
+    
+    
+    @SuppressLint("NewApi") 
+    public void openFriends(View view){    	
+    	System.out.println("In Open Friends");
+    	Intent intent = new Intent(this, DisplayActivity.class);
+    	String msg = ((EditText)(findViewById(R.id.edit_message))).getText().toString();
+    	intent.putExtra("MESSAGE", msg);
+    	startActivity(intent);
+    }
+}

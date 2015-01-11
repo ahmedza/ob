23a2f4bc1213caee096dec3fa2fb3f3f8eb922a0
Zahid Ahmed <zaid_ahmmed@hotmail.com>
2015-01-12 00:04:35 +0400

    committing login screen

--- gen/com/training/myfirstapp/R.java
+++ gen/com/training/myfirstapp/R.java
@@ -22,8 +22,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int actionbar_background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int pictuary_bg=0x7f020002;
+        public static final int actionbar_pictuary=0x7f020001;
+        public static final int bg_button=0x7f020002;
+        public static final int bg_login=0x7f020003;
+        public static final int ic_action_name=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int pictuary_bg=0x7f020006;
     }
     public static final class id {
         public static final int action_search=0x7f080006;
@@ -37,6 +41,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main_first=0x7f030000;
         public static final int display_activity_main=0x7f030001;
+        public static final int login=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -47,7 +52,9 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int button1=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int login=0x7f050007;
         public static final int message=0x7f050004;
+        public static final int signup=0x7f050006;
         public static final int title_activity_main=0x7f050005;
     }
     public static final class style {

ccf6c056608dd38a13326d1f068c3cd408bda101
Zahid Ahmed <zaid_ahmmed@hotmail.com>
2015-01-16 15:27:43 +0400

    COmmitting home work

--- gen/com/training/myfirstapp/R.java
+++ gen/com/training/myfirstapp/R.java
@@ -30,18 +30,25 @@ public final class R {
         public static final int pictuary_bg=0x7f020006;
     }
     public static final class id {
-        public static final int action_search=0x7f080006;
-        public static final int action_settings=0x7f080005;
+        public static final int TextView01=0x7f080006;
+        public static final int TextView03=0x7f080005;
+        public static final int action_search=0x7f08000c;
+        public static final int action_settings=0x7f08000b;
+        public static final int btn_signup=0x7f08000a;
         public static final int button1=0x7f080002;
         public static final int disp_tv=0x7f080004;
         public static final int edit_message=0x7f080001;
         public static final int ratingBar1=0x7f080003;
         public static final int textView1=0x7f080000;
+        public static final int txt_email=0x7f080007;
+        public static final int txt_name=0x7f080009;
+        public static final int txt_pswd=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main_first=0x7f030000;
         public static final int display_activity_main=0x7f030001;
-        public static final int login=0x7f030002;
+        public static final int login_page=0x7f030002;
+        public static final int signup_or_login=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -53,8 +60,10 @@ public final class R {
         public static final int button1=0x7f050003;
         public static final int hello_world=0x7f050002;
         public static final int login=0x7f050007;
+        public static final int login_fb=0x7f050008;
         public static final int message=0x7f050004;
         public static final int signup=0x7f050006;
+        public static final int signup_fb=0x7f050009;
         public static final int title_activity_main=0x7f050005;
     }
     public static final class style {

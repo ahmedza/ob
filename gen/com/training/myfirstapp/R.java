0bf791f1f550497d7bd2f7ece69d2861a425e3d0
Zahid Ahmed <zaid_ahmmed@hotmail.com>
2015-01-17 23:06:42 +0400

    Foot menu Frame commit. Only imageview added on layout.

--- gen/com/training/myfirstapp/R.java
+++ gen/com/training/myfirstapp/R.java
@@ -25,16 +25,21 @@ public final class R {
         public static final int actionbar_pictuary=0x7f020001;
         public static final int bg_button=0x7f020002;
         public static final int bg_login=0x7f020003;
-        public static final int ic_action_name=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int pictuary_bg=0x7f020006;
+        public static final int fm_camera=0x7f020004;
+        public static final int fm_home=0x7f020005;
+        public static final int fm_menu=0x7f020006;
+        public static final int fm_nav=0x7f020007;
+        public static final int fm_search=0x7f020008;
+        public static final int ic_action_name=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int pictuary_bg=0x7f02000b;
     }
     public static final class id {
         public static final int TextView01=0x7f080006;
         public static final int TextView03=0x7f080005;
         public static final int action_search=0x7f08000c;
         public static final int action_settings=0x7f08000b;
-        public static final int btn_signup=0x7f08000a;
+        public static final int btn_signup=0x7f080008;
         public static final int button1=0x7f080002;
         public static final int disp_tv=0x7f080004;
         public static final int edit_message=0x7f080001;
@@ -42,13 +47,14 @@ public final class R {
         public static final int textView1=0x7f080000;
         public static final int txt_email=0x7f080007;
         public static final int txt_name=0x7f080009;
-        public static final int txt_pswd=0x7f080008;
+        public static final int txt_pswd=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main_first=0x7f030000;
         public static final int display_activity_main=0x7f030001;
-        public static final int login_page=0x7f030002;
+        public static final int foot_menu=0x7f030002;
         public static final int signup_or_login=0x7f030003;
+        public static final int signup_page=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -93,7 +99,9 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
-        public static final int MyActionBar=0x7f060002;
-        public static final int MyActionBarBackGround=0x7f060003;
+        public static final int FootMenuImageView=0x7f060003;
+        public static final int MyActionBar=0x7f060004;
+        public static final int MyActionBarBackGround=0x7f060005;
+        public static final int TextFieldFont=0x7f060002;
     }
 }
